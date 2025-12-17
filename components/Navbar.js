dcd25ecdea7a94593f71a4771ad0b691d7872e94
gazeddy <33955949+gazeddy@users.noreply.github.com>
2025-12-17 15:14:24 +0000

    Add Pokédex tracker for users

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,6 +24,7 @@ export default function Navbar() {
         <div className={`nav-links ${open ? "open" : ""}`}>
           <Link href="/" className="nav-item">Home</Link>
           <Link href="/search-strings" className="nav-item">Search Builder</Link>
+          <Link href="/pokedex" className="nav-item">Pokédex</Link>
 
           {session && (
             <>
