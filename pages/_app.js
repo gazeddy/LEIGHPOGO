fbab6043788ff4d19696f84fa560ec5cb682fd58
gazeddy <33955949+gazeddy@users.noreply.github.com>
2026-07-29 11:35:38 +0100

    Show guided event ticker globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { SessionProvider } from "next-auth/react"
+import { useRouter } from "next/router"
 import "../styles/globals.css"
 import Navbar from "../components/Navbar"
+import EventTicker from "../components/events/EventTicker"
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const router = useRouter()
+  const showEventTicker = !router.pathname.startsWith("/events")
+
   return (
     <SessionProvider session={session}>
       <Navbar />
+      {showEventTicker && <EventTicker />}
       <Component {...pageProps} />
     </SessionProvider>
   )
