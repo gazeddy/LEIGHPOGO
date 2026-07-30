da943e506b81e88214c1caa2183b1273adb9f4b8
gazeddy <33955949+gazeddy@users.noreply.github.com>
2026-07-30 18:19:05 +0100

    Wire gym map styles and ticker

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { SessionProvider } from "next-auth/react"
 import { useRouter } from "next/router"
+import "leaflet/dist/leaflet.css"
 import "../styles/globals.css"
 import "../styles/navbar.css"
 import "../styles/tickers.css"
+import "../styles/gyms.css"
 import Navbar from "../components/Navbar"
 import EventTicker from "../components/events/EventTicker"
 import RaidBossTicker from "../components/events/RaidBossTicker"
+import NewGymTicker from "../components/gyms/NewGymTicker"
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter()
@@ -16,6 +19,7 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
       <Navbar />
       {showEventTicker && <EventTicker />}
       <RaidBossTicker />
+      <NewGymTicker />
       <Component {...pageProps} />
     </SessionProvider>
   )
