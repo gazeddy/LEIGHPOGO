da9bc520ad88989bea28786a3d9498d5873b0301
gazeddy <33955949+gazeddy@users.noreply.github.com>
2026-08-02 21:21:24 +0100

    Link Pokémon availability from admin navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -175,6 +175,9 @@ export default function Navbar() {
                     <Link href="/admin" className="nav-item nav-subitem">
                       Admin Panel
                     </Link>
+                    <Link href="/admin/pokedex" className="nav-item nav-subitem">
+                      Pokédex Availability
+                    </Link>
                     <Link href="/admin/events" className="nav-item nav-subitem">
                       Event Feed
                     </Link>
